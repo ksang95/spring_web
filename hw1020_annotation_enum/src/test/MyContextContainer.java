0dab1e85430afbef6ba7ae8068de25e0baddd12d
user <user@DESKTOP-76T1UM7>
2019-10-22 23:40:02 +0900

    .

--- hw1020_annotation_enum/src/test/MyContextContainer.java
+++ hw1020_annotation_enum/src/test/MyContextContainer.java
@@ -8,7 +8,7 @@ public class MyContextContainer {
 	public <T> T invokeAnnotation(T instance) {
 		Method[] methods = instance.getClass().getDeclaredMethods(); //instance의 모든 메소드들
 		
-		for (java.lang.reflect.Method method : methods) {
+		for (Method method : methods) {
 			MyAnno anno = method.getAnnotation(MyAnno.class); //메소드에 MyAnno Annotation 있으면 value 출력
 			if (anno != null)
 				System.out.println("annotation invoked == "+anno.value());
